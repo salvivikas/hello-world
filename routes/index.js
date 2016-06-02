2831aafda994b5a0dae7e46da113b62e44862bf1
salvivikas <salvi.vikas@gmail.com>
2016-06-02 13:20:45 +0530

    added app-root-path

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,63 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/index');
+var path = require('path');
+var appRoot = require('app-root-path');
+
+var rootPath = function (req, res, next) {
+  //req.rootPath = '\root';
+  req.rootPath = path.dirname(require.main.filename);
+  next();
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET home page. */
+router.get('/', function (req, res) {
+  res.sendFile(path.join(appRoot.path, 'home.html'));
+});
+
 // Get all todos
-router.get('/task', function(req, res) {
-  models.Tasks.all().then(function(taskList) {
+router.get('/task', function (req, res) {
+  models.Tasks.all().then(function (taskList) {
     //res.render('index', {tasks: taskList});
     res.json(taskList);
   });
 });
 
 // Insert
-router.post('/task', function(req, res) {
+router.post('/task', function (req, res) {
   var task = req.body.taskName;
   models.Tasks
-        .build({
-            Title: req.body.taskName,
-            Complete: false})
-        .save()
-        .then(function() {
-          res.send('success');
-        },
-        function (err) {
-          res.send('error');
-        });
+    .build({
+      Title: req.body.taskName,
+      Complete: false
+    })
+    .save()
+    .then(function () {
+      res.send('success');
+    },
+    function (err) {
+      res.send('error');
+    });
 });
 
 // Update
-router.put('/task/:id', function(req, res) {
+router.put('/task/:id', function (req, res) {
   models.Tasks.find({
     where: {
       Id: req.params.id
     }
-  }).then(function(task) {
-    if(task) {
+  }).then(function (task) {
+    if (task) {
       task.updateAttributes({
         Title: req.body.taskName,
         Complete: req.body.complete
-      }).then(function(task) {
+      }).then(function (task) {
         res.send('success');
       });
     }
@@ -50,15 +65,15 @@ router.put('/task/:id', function(req, res) {
 });
 
 // Delete
-router.delete('/task/:id', function(req, res) {
+router.delete('/task/:id', function (req, res) {
   models.Tasks.destroy({
     where: {
       id: req.params.id
     }
-  }).then(function(task) {
+  }).then(function (task) {
     res.send('success');
   }, function (err) {
-       res.send('error');
+    res.send('error');
   });
 });
 module.exports = router;
